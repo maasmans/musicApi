5aa799377bafcd43a9f7cf609dab3298f520178e
laszlo maas <laszlo.maas@teamrockstars.nl>
2022-11-16 19:49:23 +0100

    added pgsql

--- src/main/java/com/laszlo/musicapi/repository/ArtistRepository.java
+++ src/main/java/com/laszlo/musicapi/repository/ArtistRepository.java
@@ -1,11 +1,12 @@
 package com.laszlo.musicapi.repository;
 
 import com.laszlo.musicapi.model.Artist;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface ArtistRepository extends CrudRepository<Artist,Integer> {
+public interface ArtistRepository extends JpaRepository<Artist,Integer> {
     List<Artist> findAll();
     List<Artist> findByName(String name);
 }
